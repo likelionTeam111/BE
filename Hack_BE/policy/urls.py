965f7dd56dba12f0bff841d1cef0b51057ea3984
dangyulee <dg200101@gmail.com>
2025-08-11 17:02:15 +0900

    12341421

--- Hack_BE/policy/urls.py
+++ Hack_BE/policy/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import *
-from .services import load_policies
 
 app_name = 'policy'
 urlpatterns = [
     path('', Policy_list.as_view()),
-    path('load/',load_policies),
 ]
