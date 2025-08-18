fadeff6e225497dd5a45722247ce6fe3fa0c47a2
dangyulee <dg200101@gmail.com>
2025-08-18 22:02:24 +0900

    policy 관련 api 완료

--- Hack_BE/policy/serializers.py
+++ Hack_BE/policy/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+
 def _get_display(obj, field):
     """enum 필드면 get_FOO_display() 호출, 아니면 ''"""
     method = f"get_{field}_display"
@@ -12,7 +13,7 @@ def _get_display(obj, field):
 
 class ChatRequestSerializer(serializers.Serializer):
     message = serializers.CharField(allow_blank=False, trim_whitespace=True)
-    thread_id = serializers.CharField(required=False, allow_blank=True)
+    thread_id = serializers.CharField(required=False, allow_blank=True, allow_null=True)
 
 class ChatResponseSerializer(serializers.Serializer):
     answer = serializers.CharField()
@@ -23,57 +24,101 @@ class PolicySerializer(serializers.ModelSerializer):
         model = Policy
         fields = '__all__'
 
-class BriefPolicySerializer(serializers.ModelSerializer):
-    is_favorited = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Policy
-        fields = ['plcyNm', 'aiSummary', 'is_favorited']
-
-    def get_is_favorited(self,obj):
-        user = self.context['request'].user
-
-        if not user or not user.is_authenticated:
-            return False
-        
-        return Favorite_policy.objects.filter(user=user, policy=obj).exists()
-
-class DetailPolicySerializer(serializers.ModelSerializer):
-    plcyPvsnMthdCd_display = serializers.SerializerMethodField()
+    
+class PolicyInfoSerializer(serializers.ModelSerializer):
     mrgSttsCd_display = serializers.SerializerMethodField()
-    earnCndSeCd_display = serializers.SerializerMethodField()
     plcyMajorCd_display = serializers.SerializerMethodField()
     jobCd_display = serializers.SerializerMethodField()
     schoolCd_display = serializers.SerializerMethodField()
     sbizCd_display = serializers.SerializerMethodField()
 
+    is_favorited = serializers.SerializerMethodField()
+    addr = serializers.SerializerMethodField()
+    about_benefit = serializers.SerializerMethodField()
+    bizPrd = serializers.SerializerMethodField()
+    ageLmt = serializers.SerializerMethodField()
+    earnLmt = serializers.SerializerMethodField()
+    etc = serializers.SerializerMethodField()
+
     class Meta:
         model = Policy
         fields = [
-            "plcyNm", "plcyKywdNm", "lclsfNm", "mclsfNm",
-            "plcyPvsnMthdCd_display",
-            "plcyExplnCn", "plcySprtCn", "etcMttrCn",
-            "bizPrdBgngYmd", "bizPrdEndYmd", "bizPrdEtcCn",
-            "plcyAplyMthdCn", "sbmsnDcmntCn", "srngMthdCn", "aplyYmd", 
-            "aplyUrlAddr", "refUrlAddr1", "refUrlAddr2",
-            "rgtrInstCdNm",
-            
-            "sprtTrgtAgeLmtYn", "sprtTrgtMinAge", "sprtTrgtMaxAge", 
-            "mrgSttsCd_display",
-            "earnCndSeCd_display", "earnMinAmt", "earnMaxAmt", "earnEtcCn",
-            "plcyMajorCd_display",
-            "jobCd_display", 
-            "schoolCd_display",
-            "sbizCd_display",
+            # 개요
+            'plcyNm', "plcyExplnCn",  'aiSummary', 'is_favorited', 'addr', "plcyKywdNm"
+            # 지원내용
+            "about_benefit",
+            # 사업기간
+            "bizPrd",
+            # 신청관련
+            "aplyYmd", "sbmsnDcmntCn", "plcyAplyMthdCn", "srngMthdCn",  
+            #요건
+            "ageLmt", "earnLmt", "mrgSttsCd_display", "plcyMajorCd_display", "jobCd_display", "schoolCd_display", "sbizCd_display", 'etc',           
         ]
 
-    def get_plcyPvsnMthdCd_display(self, obj): return obj.get_plcyPvsnMthdCd_display()
     def get_mrgSttsCd_display(self, obj): return obj.get_mrgSttsCd_display()
-    def get_earnCndSeCd_display(self, obj): return obj.get_earnCndSeCd_display()
     def get_plcyMajorCd_display(self, obj): return obj.get_plcyMajorCd_display()
     def get_jobCd_display(self, obj): return obj.get_jobCd_display()
     def get_schoolCd_display(self, obj): return obj.get_schoolCd_display()
     def get_sbizCd_display(self, obj): return obj.get_sbizCd_display()
+
+    def get_is_favorited(self,obj):
+        request = self.context.get("request")
+        user = getattr(request, "user", None)
+
+        if not user or not user.is_authenticated:
+            return False
+        
+        return Favorite_policy.objects.filter(user=user, policy=obj).exists()
+    
+    def get_addr(self,obj):
+        if obj.aplyUrlAddr:
+            return obj.aplyUrlAddr
+        elif obj.refUrlAddr1:
+            return obj.refUrlAddr1
+        return ""
+    
+    def get_about_benefit(self,obj):
+        return f'{obj.plcySprtCn} / {obj.etcMttrCn}'
+    
+    def get_bizPrd(self, obj):
+        bizprd = ""
+        if obj.bizPrdBgngYmd != "" or obj.bizPrdEndYmd != "":
+                bizprd += f'{obj.bizPrdBgngYmd} ~ {obj.bizPrdEndYmd}'
+        if obj.bizPrdEtcCn != "":
+            if bizprd:
+                bizprd += " / "
+            bizprd += f'{obj.bizPrdEtcCn}'
+        return bizprd
+    
+    def get_ageLmt(self, obj):
+        if obj.sprtTrgtAgeLmtYn == "Y":
+            if obj.sprtTrgtMinAge != "" or obj.sprtTrgtMaxAge != "":
+                return f'{obj.sprtTrgtMinAge} ~ {obj.sprtTrgtMaxAge}'
+        else:
+            return "제한없음"
+    
+    def get_earnLmt(self, obj):
+        earnLmt = ""
+        if obj.get_earnCndSeCd_display() == "무관":
+            earnLmt += obj.get_earnCndSeCd_display()
+        else:
+            earnLmt += obj.get_earnCndSeCd_display()
+            if (obj.earnMinAmt != "0" and obj.earnMaxAmt != "0") and (obj.earnMinAmt != "" and obj.earnMaxAmt != ""):
+                earnLmt += f' / {obj.earnMinAmt} ~ {obj.earnMinAmt}'
+        if obj.earnEtcCn != "":
+            earnLmt += f' / {obj.earnEtcCn}'
+        return earnLmt
+    
+    def get_etc(self, obj):
+        etc = ""
+        if obj.addAplyQlfcCndCn != "":
+            etc += obj.addAplyQlfcCndCn
+        if obj.ptcpPrpTrgtCn != "":
+            if etc != "":
+                etc += f'\n {obj.ptcpPrpTrgtCn}'
+        return etc
+
+
 
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
--- Hack_BE/policy/urls.py
+++ Hack_BE/policy/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 app_name = 'policy'
 urlpatterns = [
-    path('', Policy_list.as_view()),
-    path("chat/", ChatView.as_view(), name="chat"),
-    path("<int:id>/brief/", Brief_policy_info.as_view(), name="brief_info"),
-    path("<int:id>/detail/", Detail_policy_info.as_view(), name="detail_info"),
-    path("favorite/<int:policy_id>/post/", Favorite_policy_View.as_view(), name="favorite_post"),
-    path("favorite/<int:policy_id>/delete/", Favorite_policy_View.as_view(), name="favorite_delete"),
-    path("favorite/list/", Favorite_policy_list_View.as_view(), name="favorite_list"),
+    path('', Policy_list_view.as_view()),
+    path("chat/", Chat_view.as_view(), name="chat"),
+    path("chat/<int:policy_id>/", Detail_chat_view.as_view(), name="detail_chat"),
+    path("<int:id>/", Policy_info_view.as_view(), name="policy_info"),
+    path("favorite/<int:policy_id>/post/", Favorite_policy_view.as_view(), name="favorite_post"),
+    path("favorite/<int:policy_id>/delete/", Favorite_policy_view.as_view(), name="favorite_delete"),
+    path("favorite/list/", Favorite_policy_list_view.as_view(), name="favorite_list"),
 ]
