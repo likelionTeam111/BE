8bc5ab5d962d5e67603058ec0930ed1bed14e659
dangyulee <dg200101@gmail.com>
2025-08-22 12:28:31 +0900

    권한 부여

--- Hack_BE/profiles/views.py
+++ Hack_BE/profiles/views.py
@@ -21,7 +21,6 @@ class Profile_view(generics.RetrieveAPIView):
         
 class Enroll_view(APIView):
     permission_classes = [IsAuthenticated]
-
     def post(self, request):
         if Profile.objects.filter(user=request.user).exists():
             return Response({"detail": "Profile already exists"}, status=status.HTTP_409_CONFLICT) 
@@ -40,60 +39,9 @@ class Enroll_view(APIView):
             serializer.data,
             status=status.HTTP_201_CREATED if created else status.HTTP_200_OK
         )
-    
-# class MyPageView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def get(self,request):
-#         # user = CustomUser.objects.first()  # 테스트용
-#         user = request.user
-        
-#         try:
-#             profile = user.profile
-#             profile_data = ProfileSerializer(profile).data
-#         except Profile.DoesNotExist:
-#             profile_data = None
-
-#         return Response(
-#             {
-#                 "profile": profile_data,
-#             },
-#             status=status.HTTP_200_OK
-#         )
-
-
-# class EnrollView(APIView):
-#     permission_classes = [IsAuthenticated]
-#     def post(self, request):
-#         # user = CustomUser.objects.first()  # 테스트용
-
-#         user = request.user
-#         profile, _ = Profile.objects.update_or_create(
-#             user=user,
-#             defaults={
-#                 "age":None,
-#                 "region":"",
-#                 "marry_code":None,
-#                 "max_income":None,
-#                 "min_income":None,
-#                 "graduate_code":None,
-#                 "employment_code":None,
-#                 "goal":"",
-#             }
-#         )
-                                                
-#         # ManyToMany 초기화
-#         profile.major_code.clear()
-#         profile.special_code.clear()
-
-#         # serializer에 데이터 그대로 넘김
-#         serializer = ProfileSerializer(profile, data=request.data, partial=False)
-#         if serializer.is_valid():
-#             serializer.save()  # LabelManyField가 자동으로 M2M 처리
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class Recommend_view(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = RecommendSerializer
     pagination_class = PageNumberPagination
     def get_queryset(self):
