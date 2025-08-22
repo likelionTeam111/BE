fdf81df75871714cfb44efa4241c9cde65cca4ce
dangyulee <dg200101@gmail.com>
2025-08-22 12:52:53 +0900

    policy info serializer 수정

--- Hack_BE/policy/urls.py
+++ Hack_BE/policy/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', Policy_list_view.as_view()),
     path("chat/", Chat_view.as_view(), name="chat"),
     path("chat/<int:policy_id>/", Detail_chat_view.as_view(), name="detail_chat"),
-    path("<int:id>/", Policy_info_view.as_view(), name="policy_info"),
+    path("info/<int:id>/", Policy_info_view.as_view(), name="policy_info"),
     path("favorite/<int:policy_id>/post/", Favorite_policy_view.as_view(), name="favorite_post"),
     path("favorite/<int:policy_id>/delete/", Favorite_policy_view.as_view(), name="favorite_delete"),
     path("favorite/list/", Favorite_policy_list_view.as_view(), name="favorite_list"),
