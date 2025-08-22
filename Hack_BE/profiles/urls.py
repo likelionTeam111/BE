28cd5345a0f5226de27a72d9decfbbf8e9f27ba2
dangyulee <dg200101@gmail.com>
2025-08-22 10:12:00 +0900

    임시

--- Hack_BE/profiles/urls.py
+++ Hack_BE/profiles/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import EnrollView, Recommend_view, Profile_view
+from .views import Recommend_view, Profile_view
 
 
 app_name = 'profiles'
 urlpatterns = [
     path("mypage/",Profile_view.as_view(),name="profile-mypage"),
-    path("enroll/",EnrollView.as_view(),name="profile-enroll"),
-    path("enroll/",EnrollView.as_view(),name="profile-enroll"),
+    # path("enroll/",EnrollView.as_view(),name="profile-enroll"),
     path("recommend/", Recommend_view.as_view()),
 ]
--- Hack_BE/profiles/views.py
+++ Hack_BE/profiles/views.py
@@ -10,6 +10,8 @@ from django.shortcuts import get_object_or_404
 from .serializers import ProfileSerializer, RecommendSerializer
 from .recommend import recommend_by_onboarding
 
+from django.core.exceptions import MultipleObjectsReturned
+
 class Profile_view(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
 
@@ -17,8 +19,11 @@ class Profile_view(generics.RetrieveAPIView):
         return Profile.objects.select_related('user')
     
     def get_object(self):
-        return get_object_or_404(self.get_queryset(), user=self.request.user)
-    
+        try:
+            return get_object_or_404(self.get_queryset(), user=self.request.user)
+        except MultipleObjectsReturned:
+            # 가장 최근 것 하나만 사용 (또는 에러로 돌려도 됨)
+            return self.get_queryset().filter(user=self.request.user).order_by('-id').first()
 # class MyPageView(APIView):
 #     permission_classes = [IsAuthenticated]
 
@@ -40,36 +45,36 @@ class Profile_view(generics.RetrieveAPIView):
 #         )
 
 
-class EnrollView(APIView):
-    permission_classes = [IsAuthenticated]
-    def post(self, request):
-        # user = CustomUser.objects.first()  # 테스트용
+# class EnrollView(APIView):
+#     permission_classes = [IsAuthenticated]
+#     def post(self, request):
+#         # user = CustomUser.objects.first()  # 테스트용
 
-        user = request.user
-        profile, _ = Profile.objects.update_or_create(
-            user=user,
-            defaults={
-                "age":None,
-                "region":"",
-                "marry_code":None,
-                "max_income":None,
-                "min_income":None,
-                "graduate_code":None,
-                "employment_code":None,
-                "goal":"",
-            }
-        )
+#         user = request.user
+#         profile, _ = Profile.objects.update_or_create(
+#             user=user,
+#             defaults={
+#                 "age":None,
+#                 "region":"",
+#                 "marry_code":None,
+#                 "max_income":None,
+#                 "min_income":None,
+#                 "graduate_code":None,
+#                 "employment_code":None,
+#                 "goal":"",
+#             }
+#         )
                                                 
-        # ManyToMany 초기화
-        profile.major_code.clear()
-        profile.special_code.clear()
+#         # ManyToMany 초기화
+#         profile.major_code.clear()
+#         profile.special_code.clear()
 
-        # serializer에 데이터 그대로 넘김
-        serializer = ProfileSerializer(profile, data=request.data, partial=False)
-        if serializer.is_valid():
-            serializer.save()  # LabelManyField가 자동으로 M2M 처리
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         # serializer에 데이터 그대로 넘김
+#         serializer = ProfileSerializer(profile, data=request.data, partial=False)
+#         if serializer.is_valid():
+#             serializer.save()  # LabelManyField가 자동으로 M2M 처리
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class Recommend_view(generics.ListAPIView):
     serializer_class = RecommendSerializer
